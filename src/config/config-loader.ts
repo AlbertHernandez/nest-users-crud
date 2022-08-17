9d4e5c3a881f6ec3456d73a1e1e4fbc215ff29f1
Albert Hernandez <alberthernandezdev@gmail.com>
2022-08-17 13:22:51 +0200

    feat: add environment

--- src/config/config-loader.ts
+++ src/config/config-loader.ts
@@ -2,5 +2,6 @@ export const configLoader = () => {
   return {
     port: process.env.PORT,
     apiKey: process.env.API_KEY,
+    environment: process.env.NODE_ENV,
   };
 };
--- src/config/env-schema.ts
+++ src/config/env-schema.ts
@@ -3,4 +3,5 @@ import * as Joi from 'joi';
 export const envSchema = Joi.object({
   PORT: Joi.string().default(3000),
   API_KEY: Joi.string().required(),
+  NODE_ENV: Joi.string().default('development'),
 });
