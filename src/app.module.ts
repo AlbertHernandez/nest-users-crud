7b0fdc6cfc959963ffcf29ac251c062aadb654a2
Albert Hernandez <alberthernandezdev@gmail.com>
2022-08-19 15:46:45 +0200

    feat: create user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,4 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UsersController } from './users/users.controller';
-import { UsersService } from './users/users.service';
 import { LoggerModule } from 'nestjs-pino';
 import { CorrelationIdMiddleware } from './correlation-id.middleware';
 import { ConfigModule } from '@nestjs/config';
@@ -12,20 +8,19 @@ import { AuthModule } from './auth/auth.module';
 import { SentryInterceptor, SentryModule } from '@ntegral/nestjs-sentry';
 import { APP_INTERCEPTOR } from '@nestjs/core';
 import { SentryMiddleware } from './sentry.middleware';
+import { UserModule } from './users/user.module';
 import { sentryOptions } from './sentry/sentry-options';
 import { sentryInterceptorOptions } from './sentry/sentry-interceptor-options';
 
 @Module({
   imports: [
+    UserModule,
     LoggerModule.forRoot(loggerOptions),
     ConfigModule.forRoot(configOptions),
     AuthModule,
     SentryModule.forRootAsync(sentryOptions),
   ],
-  controllers: [AppController, UsersController],
   providers: [
-    AppService,
-    UsersService,
     ConfigModule,
     {
       provide: APP_INTERCEPTOR,
