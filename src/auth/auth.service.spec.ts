c8e8aa8e6eebfc49593c80de85d7365804f2125d
Albert Hernandez Pellicer <alberto.pellicer@coverwallet.com>
2023-12-31 18:03:46 +0100

    fix tests

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,12 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { ConfigModule } from '@nestjs/config';
 
 describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      imports: [ConfigModule],
+      providers: [AuthService, ConfigModule],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,12 +1,19 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { USER_REPOSITORY } from './user.repository';
 
 describe('UsersService', () => {
   let service: UsersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        {
+          provide: USER_REPOSITORY,
+          useValue: {},
+        },
+      ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
